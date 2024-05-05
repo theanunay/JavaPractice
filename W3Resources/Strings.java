16375cbe21f8a6df614e59cb72a22262ba0da156
AnuNay SiNgh <65433948+theanunay@users.noreply.github.com>
2024-05-05 18:02:55 +0530

    Update Strings.java

--- W3Resources/Strings.java
+++ W3Resources/Strings.java
@@ -1,3 +1,16 @@
 public class Strings {
+    public static void main(String[] args) {
+        // Q. Print all substrings of given String.
+      String str = "abcd";
+
+      for (int i=0; i<=3; i++){
+          for (int j=i+1; j<=4; j++){
+              System.out.print(str.substring(i,j)+" " );
+          }
+          System.out.println();
+      }
+
+
+    }
     
 }
